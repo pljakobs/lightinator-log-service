f842e9df068f5d58a50b6a5902b2b09a51ef9a42
Peter Jakobs <pjakobs@mobileme.de>
2026-09-26 19:44:53 +0200

    make sure the tag name is lowercase

--- src/crashDecoder.js
+++ src/crashDecoder.js
@@ -506,7 +506,7 @@ class CrashDecoder {
 
     const smingPath = await this.harvester.ensureRepo("Sming", "https://github.com/pljakobs/Sming.git", "develop");
 
-    const fwRepoPath = await this.harvester.ensureRepo("esp-rgbww-firmware", "https://github.com/pljakobs/esp_rgbww_firmware.git", git_version);
+    const fwRepoPath = await this.harvester.ensureRepo("esp-rgbww-firmware", "https://github.com/pljakobs/esp_rgbww_firmware.git", git_version ? git_version.toLowerCase() : null);
 
     const mapSymbols = await this.harvester.fetchMapFile(git_version, socKey, type);
     const codeSnippets = await this.harvester.extractSnippets(decoded, { Sming: smingPath, "esp-rgbww-firmware": fwRepoPath });
