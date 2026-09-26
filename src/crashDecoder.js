24d8680413ad7594b2ea061398e26d296188f061
Peter Jakobs <pjakobs@mobileme.de>
2026-09-26 19:20:29 +0200

    still more repo url and branch fixes

--- src/crashDecoder.js
+++ src/crashDecoder.js
@@ -504,8 +504,8 @@ class CrashDecoder {
       throw new Error("AI service is not configured.");
     }
 
-    const smingPath = await this.harvester.ensureRepo("Sming", "https://github.com/SmingHub/Sming.git", branch);
-    const fwRepoPath = await this.harvester.ensureRepo("esp-rgbww-firmware", "https://github.com/Lightinator/esp-rgbww-firmware.git", branch);
+    const smingPath = await this.harvester.ensureRepo("Sming", "https://github.com/pljakobs/Sming.git", "develop");
+    const fwRepoPath = await this.harvester.ensureRepo("esp-rgbww-firmware", "https://github.com/pljakobs/esp-rgbww-firmware.git", branch);
 
     const mapSymbols = await this.harvester.fetchMapFile(git_version, socKey, type);
     const codeSnippets = await this.harvester.extractSnippets(decoded, { Sming: smingPath, "esp-rgbww-firmware": fwRepoPath });
