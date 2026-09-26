489e9045d59e55458784b7841db01a4872a5b3db
Peter Jakobs <pjakobs@mobileme.de>
2026-09-26 16:49:55 +0200

    finally (I hope) fix the Sming repo and branch

--- src/crashDecoder.js
+++ src/crashDecoder.js
@@ -258,8 +258,8 @@ class CrashDecoder {
         console.log(`CrashDecoder [${ip}]: Initiating multi-pass AI analysis...`);
         
         // Sync Repositories
-        const smingPath = await this.harvester.ensureRepo("Sming", "https://github.com/SmingHub/Sming.git", branch);
-        const fwRepoPath = await this.harvester.ensureRepo("esp-rgbww-firmware", "https://github.com/Lightinator/esp-rgbww-firmware.git", branch);
+        const smingPath = await this.harvester.ensureRepo("Sming", "https://github.com/pljakobs/Sming.git", "develop");
+        const fwRepoPath = await this.harvester.ensureRepo("esp-rgbww-firmware", "https://github.com/pljakobs/esp-rgbww-firmware.git", branch);
         
         const mapSymbols = await this.harvester.fetchMapFile(git_version, socKey, type);
         const codeSnippets = await this.harvester.extractSnippets(decoded, { Sming: smingPath, "esp-rgbww-firmware": fwRepoPath });
